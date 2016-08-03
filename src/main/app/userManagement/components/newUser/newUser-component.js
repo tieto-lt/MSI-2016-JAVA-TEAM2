345bb8cd8fd01f34a0799e4948d345b8749ffc6d
Robertas Lasys <robertas.lasys@egmail.com>
2016-08-03 15:59:15 +0300

    some changes in newUser form

--- src/main/app/userManagement/components/newUser/newUser-component.js
+++ src/main/app/userManagement/components/newUser/newUser-component.js
@@ -1,6 +1,23 @@
 var module = require('main_module');
 
 function Controller($state, $stateParams, ItemService) {
+  var vm = this;
+  vm.username = undefined;
+  vm.password = undefined;
+
+  vm.login = login;
+  vm.error = undefined;
+
+  function login() {
+      AuthService.login(vm.username, vm.password).then(
+          function (response) {
+              vm.error = undefined;
+              $state.go('root.itemList');
+          },
+          function (err) {
+              vm.error = err.data.error_description;
+          });
+  }
 
 }
 
