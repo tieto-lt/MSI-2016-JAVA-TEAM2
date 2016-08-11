1b85d48f6c302eb903462c0c50d7b978f43ae55b
Robertas Lasys <robertas.lasys@egmail.com>
2016-08-11 14:18:56 +0300

    fixed registration form error

--- src/main/app/auth/components/login/login_component.js
+++ src/main/app/auth/components/login/login_component.js
@@ -29,10 +29,10 @@ function Controller($state, AuthService, $stateParams, Session, OperatorService)
                     console.log(response.data);
                     if(response.data.token){
                         vm.error = undefined;
-                        $state.go('root.operatorPage');
+                        $state.go('root.operatorHomePage');
                     } else {
                         vm.error = undefined;
-                        $state.go('root.operatorHomePage');
+                        $state.go('root.operatorPage');
                     }
                   });
                 } else {
