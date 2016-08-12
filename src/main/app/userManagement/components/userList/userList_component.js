fc59cec2656c3722522677d5b795de8855a5f57f
Robertas Lasys <robertas.lasys@egmail.com>
2016-08-12 10:38:03 +0300

    images fix and remove userList

--- src/main/app/userManagement/components/userList/userList_component.js
+++ src/main/app/userManagement/components/userList/userList_component.js
@@ -1,80 +1,40 @@
 var module = require('main_module');
 require('userList.scss');
 
-function Controller(UserServiceImpl, $uibModal, $scope) {
-  //Convention to call controller instance 'vm'
-  var vm = this;
+function Controller(UserServiceImpl) {
+    //Convention to call controller instance 'vm'
+    var vm = this;
 
-  vm.user = {};
-
-  vm.open = open;
-  vm.close = close;
-  vm.wait = wait;
-  vm.executeUpdate = executeUpdate;
-
-  $scope.items = ['item1', 'item2', 'item3'];
+    vm.user = {};
 
 
-  vm.$onInit = function() {
-    UserServiceImpl.all().then(function(response) {
-      vm.users = response.data;
-    });
-  };
+    vm.executeUpdate = executeUpdate;
 
-  function wait(ms) {
-    var start = new Date().getTime();
-    var end = start;
-    while (end < start + ms) {
-      end = new Date().getTime();
-    }
-  }
-
-  function open(size) {
-    var modalInstance = $uibModal.open({
-      animation: true,
-      templateUrl: 'myModalContent.html',
-      controller: 'ModalInstanceCtrl',
-      size: "sm",
-      resolve: {
-        items: function() {
-          return $scope.items;
-        }
-      }
-    });
-
-
-  }
-
-  function close() {
-    $uibModalInstance.dismiss('cancel');
-  }
-
-  function executeUpdate(user, newRole) {
-    vm.user = user;
-    user.userRole = newRole;
-    UserServiceImpl.put(vm.user).then(
-      function() {
-        console.log('Update success');
-
-        // open();
-        // $uibModalInstance.dismiss('cancel');
-        // wait(500);
-
-      },
-      function(err) {
-        if (err.status === 400) {
-          vm.errors = err.data;
-        } else {
-          console.log('Error', err);
-        }
+    vm.$onInit = function() {
+      UserServiceImpl.all().then(function(response){
+        vm.users = response.data;
       });
-  }
+    };
+
+    function executeUpdate(user,newRole) {
+      vm.user = user;
+      user.userRole = newRole;
+        UserServiceImpl.put(vm.user).then(
+            function () { console.log('Update success'); },
+            function (err) {
+                if (err.status === 400) {
+                    vm.errors = err.data;
+                } else {
+                    console.log('Error', err);
+                }
+            });
+    }
 
 }
 
-Controller.$inject = ['UserServiceImpl', '$uibModal', '$scope'];
+Controller.$inject = ['UserServiceImpl'];
 
 module.component('userList', {
-  controller: Controller,
-  templateUrl: require('./userList.html')
+    controller: Controller,
+    templateUrl: require('./userList.html')
 });
--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
             allChunks: true
          }),
          new CopyWebpackPlugin([
-          { from: { glob: './src/main/app/Images/**/*.ico'}},
+          { from: { glob: './src/main/app/Images/**/*.*'}},
         ])
      ],
      resolve: {
