16786e39d4e8cb4bafce5d50208453f1b940c586
Ana Dobzinska <anica995@gmail.com>
2016-08-25 15:55:07 +0300

    Design # Conflicts: #	src/main/app/userManagement/components/accountInformation/accountInformation.html

--- src/main/app/userManagement/components/homePage/homePage-component.js
+++ src/main/app/userManagement/components/homePage/homePage-component.js
@@ -2,27 +2,26 @@ var module = require('main_module');
 require('style.scss');
 
 function Controller($state, $stateParams, ItemService) {
-     var vm = this;
+    var vm = this;
     var vid = document.getElementById("bgvid");
     var pauseButton = document.querySelector("#polina button");
-    var detailsVisible=false;
+    var detailsVisible = false;
     vm.showDetails = showDetails;
 
     function vidFade() {
-      vid.classList.add("stopfade");
+        vid.classList.add("stopfade");
     }
 
-    vid.addEventListener('ended', function()
-    {
-    // only functional if "loop" is removed
-    vid.pause();
-    // to capture IE10
-    vidFade();
+    vid.addEventListener('ended', function() {
+        // only functional if "loop" is removed
+        vid.pause();
+        // to capture IE10
+        vidFade();
     });
 
-    function showDetails(){
-    vm.detailsVisible= !vm.detailsVisible;
-    return  vm.detailsVisible;
+    function showDetails() {
+        vm.detailsVisible = !vm.detailsVisible;
+        return vm.detailsVisible;
     }
 }
 
